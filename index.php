5ec1555fcbb106fc90a81f14d9088fd4e5299088
Jarvis Westen <contact@thebliveon.com>
2015-04-19 22:32:05 +0200

    Bootstrap Uninstall

--- index.php
+++ index.php
@@ -10,10 +10,7 @@ $page = $_GET["page"];
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="css/pace.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	
-	
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script src="javascript/jquery.js"></script>
 	<script src="javascript/pace.js"></script>
 	<script type="text/javascript"> 
