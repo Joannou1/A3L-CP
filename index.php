2bf0d403133520fb462b88986213042e525acec1
Jarvis Westen <contact@thebliveon.com>
2015-04-21 18:12:30 +0200

    yolo

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $page = $_GET["page"];
 	<link rel="shortcut icon" href="favicon.ico">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="css/pace.css">
+	<link rel="stylesheet" href="css/external.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	
 	<script src="javascript/jquery.js"></script>
